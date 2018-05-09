a02ced083229790af990ff5218a188aa49419520
Binu <binu.webaxyz@gmail.com>
2018-05-09 09:52:55 +0530

    bootstrap install by binu

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule,MatToolbarModule],
-  exports: [MatButtonModule, MatCheckboxModule,MatToolbarModule],
+  imports: [MatButtonModule, MatCheckboxModule,MatToolbarModule,MatGridListModule,MatIconModule,MatTabsModule,MatCardModule],
+  exports: [MatButtonModule, MatCheckboxModule,MatToolbarModule,MatGridListModule,MatIconModule,MatTabsModule,MatCardModule],
 })
 
 export class ComponentsModule { }
