88934f388524cf93c7e3f04aa89f997af564e62b
bhuvana <bhuvana@webaxyz.com>
2018-05-17 18:41:21 +0530

    boo

--- src/app/bookingpage/booking/booking.component.ts
+++ src/app/bookingpage/booking/booking.component.ts
@@ -1,15 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-booking',
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './booking.component.html',
   styleUrls: ['./booking.component.scss']
 })
 export class BookingComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit() {
   }
+
+  tab1: string = 'tabD';
+  tab2: string = 'tabN';
+  tab3: string = 'tabN';
+
+
+  trvl_inf(event) {
+    let length = event.target.classList.length;
+    length = length - 1;
+    let className = event.target.classList[length];
+    if (className == 'tab1') {
+      this.tab2 = 'tabN';
+      this.tab3 = 'tabN';
+      if ((this.tab2 === 'tabN') && (this.tab3 === 'tabN')) {
+        this.tab1 = 'tabD';
+      }
+      else {
+        this.tab1 = this.tab1 === 'tabN' ? 'tabD' : 'tabN';
+      }
+
+    }
+    if (className == 'tab2') {
+      this.tab1 = 'tabN';
+      this.tab3 = 'tabN';
+      if ((this.tab1 === 'tabN') && (this.tab3 === 'tabN')) {
+        this.tab2 = 'tabD';
+      }
+      else {
+        this.tab2 = this.tab2 === 'tabN' ? 'tabD' : 'tabN';
+      }
+
+    }
+    if (className == 'tab3') {
+      
+      this.tab1 = 'tabN';
+      this.tab2 = 'tabN';
+      if ((this.tab1 == 'tabN') && (this.tab2 == 'tabN')) {
+        this.tab3 = 'tabD';
+      }
+      else {
+        this.tab3 = this.tab3 === 'tabN' ? 'tabD' : 'tabN';
+      }
+
+
+    }
+  }
+
 
 }
