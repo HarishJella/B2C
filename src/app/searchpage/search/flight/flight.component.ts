cf373b2188d0f2151204fa92e2589a8355e9edc9
harish <harish@hexwhale.com>
2018-05-18 08:11:31 +0530

    harish

--- src/app/searchpage/search/flight/flight.component.ts
+++ src/app/searchpage/search/flight/flight.component.ts
@@ -11,15 +11,14 @@ export class FlightComponent implements OnInit {
 
   ngOnInit() {
   }
+
   formatLabel(value: number | null) {
     if (!value) {
       return 0;
     }
-
     if (value >= 1000) {
       return Math.round(value / 1000) + 'k';
     }
-
     return value;
   }
 
@@ -30,34 +29,24 @@ export class FlightComponent implements OnInit {
   airlineState: string = 'filter_content';
   dynamicClass: string = 'col-10';
 
-
-
   open_filter(event) {
     let length = event.target.classList.length;
     length = length - 1;
     let className = event.target.classList[length];
     console.log(className);
     if (className == 'priceState') {
-
-
-
       this.priceState = this.priceState === 'filter_content' ? 'fil' : 'filter_content';
       if (this.priceState == 'filter_content') {
         this.dynamicClass = 'col-10';
       } else {
         this.dynamicClass = 'col-8';
       }
-
       this.deptState = 'filter_content';
       this.stopsState = 'filter_content';
       this.fairState = 'filter_content';
       this.airlineState = 'filter_content';
-
     }
     if (className == 'deptState') {
-
-
-
       this.deptState = this.deptState === 'filter_content' ? 'fil' : 'filter_content';
       if (this.deptState == 'filter_content') {
         this.dynamicClass = 'col-10';
@@ -70,8 +59,6 @@ export class FlightComponent implements OnInit {
       this.airlineState = 'filter_content';
     }
     if (className == 'stopsState') {
-
-
       this.stopsState = this.stopsState === 'filter_content' ? 'fil' : 'filter_content';
       if (this.stopsState == 'filter_content') {
         this.dynamicClass = 'col-10';
@@ -84,8 +71,6 @@ export class FlightComponent implements OnInit {
       this.airlineState = 'filter_content';
     }
     if (className == 'fairState') {
-
-
       this.fairState = this.fairState === 'filter_content' ? 'fil' : 'filter_content';
       if (this.fairState == 'filter_content') {
         this.dynamicClass = 'col-10';
@@ -98,7 +83,6 @@ export class FlightComponent implements OnInit {
       this.airlineState = 'filter_content';
     }
     if (className == 'airlineState') {
-
       this.airlineState = this.airlineState === 'filter_content' ? 'fil' : 'filter_content';
       if (this.airlineState == 'filter_content') {
         this.dynamicClass = 'col-10';
@@ -109,15 +93,8 @@ export class FlightComponent implements OnInit {
       this.deptState = 'filter_content';
       this.stopsState = 'filter_content';
       this.fairState = 'filter_content';
-
     }
-
     // console.log(className);
     // this.menuState = this.menuState === 'filter_content' ? 'fil' : 'filter_content';
-
-
   }
-
-
-
 }
--- src/app/searchpage/search/hotel/hotel.component.ts
+++ src/app/searchpage/search/hotel/hotel.component.ts
@@ -28,65 +28,86 @@ export class HotelComponent implements OnInit {
   locationState: string = 'filter_content';
   ratingState: string = 'filter_content';
   propertyTypeState: string = 'filter_content';
-
-
+  dynamicClass: string = 'col-11';
+  hotelresult: string = 'hotelresult3';
 
   open_filter(event) {
     let length = event.target.classList.length;
     length = length - 1;
     let className = event.target.classList[length];
-
+    console.log(className);
     if (className == 'priceState') {
       this.priceState = this.priceState === 'filter_content' ? 'fil' : 'filter_content';
-
-
+      if (this.priceState == 'filter_content') {
+        this.dynamicClass = 'col-11';
+        this.hotelresult = 'hotelresult3';
+      } else {
+        this.dynamicClass = 'col-9';
+        this.hotelresult = 'hotelresult2';
+      }
       this.amenitiesState = 'filter_content';
       this.locationState = 'filter_content';
       this.ratingState = 'filter_content';
       this.propertyTypeState = 'filter_content';
-
     }
     if (className == 'amenitiesState') {
       this.amenitiesState = this.amenitiesState === 'filter_content' ? 'fil' : 'filter_content';
-
+      if (this.amenitiesState == 'filter_content') {
+        this.dynamicClass = 'col-11';
+        this.hotelresult = 'hotelresult3';
+      } else {
+        this.dynamicClass = 'col-9';
+        this.hotelresult = 'hotelresult2';
+      }
       this.priceState = 'filter_content';
-
       this.locationState = 'filter_content';
       this.ratingState = 'filter_content';
       this.propertyTypeState = 'filter_content';
     }
     if (className == 'locationState') {
       this.locationState = this.locationState === 'filter_content' ? 'fil' : 'filter_content';
-
+      if (this.locationState == 'filter_content') {
+        this.dynamicClass = 'col-11';
+        this.hotelresult = 'hotelresult3';
+      } else {
+        this.dynamicClass = 'col-9';
+        this.hotelresult = 'hotelresult2';
+      }
       this.priceState = 'filter_content';
       this.amenitiesState = 'filter_content';
-
       this.ratingState = 'filter_content';
       this.propertyTypeState = 'filter_content';
     }
     if (className == 'ratingState') {
       this.ratingState = this.ratingState === 'filter_content' ? 'fil' : 'filter_content';
-
+      if (this.ratingState == 'filter_content') {
+        this.dynamicClass = 'col-11';
+        this.hotelresult = 'hotelresult3';
+      } else {
+        this.dynamicClass = 'col-9';
+        this.hotelresult = 'hotelresult2';
+      }
       this.priceState = 'filter_content';
       this.amenitiesState = 'filter_content';
       this.locationState = 'filter_content';
-
       this.propertyTypeState = 'filter_content';
     }
     if (className == 'propertyTypeState') {
       this.propertyTypeState = this.propertyTypeState === 'filter_content' ? 'fil' : 'filter_content';
-
+      if (this.propertyTypeState == 'filter_content') {
+        this.dynamicClass = 'col-11';
+        this.hotelresult = 'hotelresult3';
+      } else {
+        this.dynamicClass = 'col-9';
+        this.hotelresult = 'hotelresult2';
+      }
       this.priceState = 'filter_content';
       this.amenitiesState = 'filter_content';
       this.locationState = 'filter_content';
       this.ratingState = 'filter_content';
-
-
     }
-
     // console.log(className);
     // this.menuState = this.menuState === 'filter_content' ? 'fil' : 'filter_content';
-
-
   }
 }
+
