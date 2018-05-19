eb7e46021d5d13cdbdb78827bd9fa79cb1b59413
bhuvana <bhuvana@webaxyz.com>
2018-05-19 17:33:54 +0530

    boo

--- src/app/searchpage/Templates/bus-template/bus-template.component.ts
+++ src/app/searchpage/Templates/bus-template/bus-template.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-bus-template',
   templateUrl: './bus-template.component.html',
-  styleUrls: ['./bus-template.component.scss']
+  styleUrls: ['./bus-template.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class BusTemplateComponent implements OnInit {
 
