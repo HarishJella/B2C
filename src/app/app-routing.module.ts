098887321c29c16833ab622bd4b5a4ee1874803b
Binu <binu.webaxyz@gmail.com>
2018-05-24 13:07:39 +0530

    harish

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { BusComponent } from './searchpage/search/bus/bus.component';
 import { HotelComponent } from './searchpage/search/hotel/hotel.component';
 import { FlightComponent } from './searchpage/search/flight/flight.component';
 import { BookingComponent } from './bookingpage/booking/booking.component';
+import { FlightbookingComponent } from './bookingpage/flightbooking/flightbooking.component';
 import { AboutComponent } from './aboutpage/about/about.component';
 import { FaqComponent } from './FAQpage/faq/faq.component';
 import { GlobalcomponentsComponent } from './globalcomponents/globalcomponents.component';
@@ -17,6 +18,7 @@ const routes: Routes = [
   { path: 'bus', component: BusComponent },
   { path: 'hotel', component: HotelComponent },
   { path: 'flight', component: FlightComponent },
+  { path: 'flight_booking', component: FlightbookingComponent },
   { path: 'booking', component: BookingComponent },
   { path: 'about', component: AboutComponent },
   { path: 'faq', component: FaqComponent },
